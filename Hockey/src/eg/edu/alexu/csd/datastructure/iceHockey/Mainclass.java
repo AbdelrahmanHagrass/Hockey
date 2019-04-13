e0ad0acff9f6215394697038c7ca3af97418d48d
AbdelrahmanHagrass <48356468+AbdelrahmanHagrass@users.noreply.github.com>
2019-04-14 00:10:01 +0200

    Update Mainclass.java

--- Hockey/src/eg/edu/alexu/csd/datastructure/iceHockey/Mainclass.java
+++ Hockey/src/eg/edu/alexu/csd/datastructure/iceHockey/Mainclass.java
@@ -7,19 +7,19 @@ import java.util.*;
 public class Mainclass {
 
 	public static void main(String[] args) {
+		/*
 		Scanner scan = new Scanner(System.in);
 		String[] s = new String[1000];
-		s[1] = scan.nextLine();
 		int team,threshold;
-		int i = 1;
-		while (s[i].charAt(s[i].length() - 1) == ',') {
-			i++;
-			s[i] = scan.nextLine();
+		for(int i=1;i<=10;i++)
+		{
+			s[i]=scan.nextLine();
 		}
 		team=scan.nextInt();
 		threshold=scan.nextInt();
 		IPlayersFinder f=new photo();
 		f.findPlayers(s, team, threshold);
+		*/
 		
 	}
 
